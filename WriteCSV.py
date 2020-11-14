8a907a6ded45a9b26844029f9e2e65f48e3e3b40
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-11-14 22:25:14 +0100

    WriteCSV : correction encodage utf-8 main.py : boucles Création des csv par catégories

--- WriteCSV.py
+++ WriteCSV.py
@@ -2,17 +2,18 @@ import csv
 
 
 def init_category_csv(category):
-    with open('CSV/'+str(category)+'.csv', 'w', newline='') as csvfile:
+    with open('CSV/'+str(category)+'.csv', 'w', newline='', encoding="utf-8") as csvfile:
         fieldnames = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
         writer.writeheader()
 
 
 def write_csv(dico, category):
-    with open('CSV/'+str(category)+'.csv', 'w', newline='') as csvfile:
+    with open('CSV/'+str(category)+'.csv', 'a', newline='', encoding="utf-8") as csvfile:
         fieldnames = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
         writer.writerow(dico)
+
 
 
 init_category_csv('Crime')
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import Scraping
 import UrlCategory
-
+import WriteCSV
 
 home = "http://books.toscrape.com/index.html"
-
-
-
 print(UrlCategory.scrap_category(home))
 
-for category in UrlCategory.scrap_category(home) :
-    u = UrlCategory.browse_page(UrlCategory.scrap_category(home)[category])
-    for livre in u :
-        print(Scraping.extract(u[livre],category))
+for category in UrlCategory.scrap_category(home):
+    WriteCSV.init_category_csv(category)
+    url_category = UrlCategory.browse_page(UrlCategory.scrap_category(home)[category])
+    for livre in url_category:
+        WriteCSV.write_csv(Scraping.extract(url_category[livre], category), category)
