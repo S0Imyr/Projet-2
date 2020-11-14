efc0fb24d6319061365eb0b8ee1dd71dbc386373
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-11-14 15:00:36 +0100

    main.py : appels de fonction du module UrlCategory

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import Scraping
 import UrlCategory
 
+home = "http://books.toscrape.com/index.html"
+
+
+
 print(Scraping.extract("http://books.toscrape.com/catalogue"
               "/its-only-the-himalayas_981/index.html", "Travel"))
 
-print()
+print(UrlCategory.scrap_category(home))
