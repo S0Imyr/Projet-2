27130d98787203c3b1a0303023a033e66aedfdbc
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-11-14 13:16:24 +0100

    UrlCategory.py : fonction qui prend les liens et livres d'une page

--- UrlCategory.py
+++ UrlCategory.py
@@ -6,7 +6,7 @@ home = "http://books.toscrape.com/index.html"
 
 # Recherche des catégories
 categories_url = {}
-soup = requete.requete_text(home)
+soup_home = requete.requete_text(home)
 
 
 def category_no_space(texte):
@@ -16,13 +16,23 @@ def category_no_space(texte):
 
 
 # def scrap_category(category,url):
-secteur = soup.find("ul", {"class": "nav nav-list"}).find("li").find("li").find("a")
+secteur = soup_home.find("ul", {"class": "nav nav-list"}).find("li").find("li").find("a")
 k = 5
 while k == 5:   # S'adapte si nouvelles catégories
     categories_url[category_no_space(secteur)] = "http://books.toscrape.com/"+secteur.get("href")
     secteur = secteur.find_next("a")
     k = len(secteur.text.split("\n"))
-# fonction Scraper page url
+
+
+def page_livre_url(soupe):  # fonction Scraper les livres et leurs url pour une page
+    reponse = {}
+    livre = soupe.find("h3")
+    test = True
+    while test:
+        reponse[livre.find("a").get("title")] = livre.find("a").get("href")
+        test = livre.find_next("h3") is not None
+        livre = livre.find_next("h3")
+    return reponse
 
 
 def browse_page(url):   # Itérer les pages
