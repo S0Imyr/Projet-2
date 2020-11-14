92093d242189b863e6149dea82122afbb333bea5
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-11-14 11:45:01 +0100

    UrlCategory.py : PEP8

--- UrlCategory.py
+++ UrlCategory.py
@@ -14,6 +14,7 @@ def category_no_space(texte):
     category = category[2].replace(" ", "")
     return category
 
+
 # def scrap_category(category,url):
 secteur = soup.find("ul", {"class": "nav nav-list"}).find("li").find("li").find("a")
 k = 5
@@ -21,22 +22,16 @@ while k == 5:   # S'adapte si nouvelles catégories
     categories_url[category_no_space(secteur)] = "http://books.toscrape.com/"+secteur.get("href")
     secteur = secteur.find_next("a")
     k = len(secteur.text.split("\n"))
-
 # fonction Scraper page url
 
 
-
-# Itérer les pages
-
-def browse_page(url):
+def browse_page(url):   # Itérer les pages
     url_base = url[:-10]
     i = 1
     test = True
     while test:
         soup = requete.requete_text(url)
-        #Scrap des liens
+        # Scrap des liens
         test = soup.find("li", {"class": "next"}) is not None
         i += 1
         url = url_base + "page-" + str(i)+".html"
-
-
