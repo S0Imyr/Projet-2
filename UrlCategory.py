023e178363d2f29275c45e891c3d9316f5ef3ae4
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-11-14 14:59:35 +0100

    UrlCategory.py : nettoyage et définition de scrap_category

--- UrlCategory.py
+++ UrlCategory.py
@@ -2,26 +2,22 @@
 import requete
 
 
-home = "http://books.toscrape.com/index.html"
-
-# Recherche des catégories
-categories_url = {}
-soup_home = requete.requete_text(home)
-
-
 def category_no_space(texte):
     category = texte.text.split("\n")
     category = category[2].replace(" ", "")
     return category
 
 
-# def scrap_category(category,url):
-secteur = soup_home.find("ul", {"class": "nav nav-list"}).find("li").find("li").find("a")
-k = 5
-while k == 5:   # S'adapte si nouvelles catégories
-    categories_url[category_no_space(secteur)] = "http://books.toscrape.com/"+secteur.get("href")
-    secteur = secteur.find_next("a")
-    k = len(secteur.text.split("\n"))
+def scrap_category(url_home):  # Recherche des catégories
+    categories_url = {}
+    soup_home = requete.requete_text(url_home)
+    secteur = soup_home.find("ul", {"class": "nav nav-list"}).find("li").find("li").find("a")
+    k = 5
+    while k == 5:   # S'adapte si nouvelles catégories
+        categories_url[category_no_space(secteur)] = "http://books.toscrape.com/"+secteur.get("href")
+        secteur = secteur.find_next("a")
+        k = len(secteur.text.split("\n"))
+    return categories_url
 
 
 def page_livre_url(soupe):  # fonction Scraper les livres et leurs url pour une page
