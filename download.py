7f080c6e70886adba145f32be7ffcbb039d29af0
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-11-15 19:36:32 +0100

    download.py : création d'un fichier avec les fonctions créant les csv et téléchargeant les images main.py : suppression des boucles et appels des fonctions à la place

---
+++ download.py
@@ -0,0 +1,30 @@
+import Scraping
+import UrlCategory
+import Write_csv
+import urllib
+
+
+def create_csv(home):
+    for category in UrlCategory.scrap_category(home):
+        Write_csv.init_category_csv(category)
+        url_category = UrlCategory.browse_page(
+            UrlCategory.scrap_category(home)[category])
+        for livre in url_category:
+            Write_csv.write_csv(Scraping.extract(
+                url_category[livre], category), category)
+
+
+def images(home):
+    for category in UrlCategory.scrap_category(home):
+        url_category = UrlCategory.browse_page(
+            UrlCategory.scrap_category(home)[category])
+        for livre in url_category:
+            format_title = str(livre).replace("/", "").replace(
+                '\\', "").replace(":", "").replace(
+                "*", "").replace("\"", "").replace(
+                "<", "").replace(">", "").replace(
+                "?", "").replace("|", "")  # caractère nom de fichiers
+            urllib.request.urlretrieve(
+                Scraping.extract(url_category[livre], category)
+                ['image_url'], "Images/" + format_title[:min(
+                    50, len(str(format_title)))] + ".jpg")
--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-import Scraping
 import UrlCategory
-import Write_csv
-import urllib
+import download
 
 home = "http://books.toscrape.com/index.html"
 print(UrlCategory.scrap_category(home))
 
-for category in UrlCategory.scrap_category(home):
-    Write_csv.init_category_csv(category)
-    url_category = UrlCategory.browse_page(
-        UrlCategory.scrap_category(home)[category])
-    for livre in url_category:
-        Write_csv.write_csv(Scraping.extract(
-            url_category[livre], category), category)
+download.images(home)
+download.create_csv(home)
 
-for category in UrlCategory.scrap_category(home):
-    url_category = UrlCategory.browse_page(
-        UrlCategory.scrap_category(home)[category])
-    for livre in url_category:
-        format_title = str(livre).replace("/", "").replace(
-            '\\', "").replace(":", "").replace(
-            "*", "").replace("\"", "").replace(
-            "<", "").replace(">", "").replace(
-            "?", "").replace("|", "")  # caractère nom de fichiers
-        urllib.request.urlretrieve(
-            Scraping.extract(url_category[livre], category)
-            ['image_url'], "Images/" + format_title[:min(
-                50, len(str(format_title)))] + ".jpg")
