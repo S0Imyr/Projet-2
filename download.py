da8ca5c8fdf600fa6c8dd5a926ae054a5eb5e9f6
rlossec <raphaelossec@gmail.com>
2023-09-26 22:32:11 +0200

    Improve docstring and add typing

--- download.py
+++ download.py
@@ -1,50 +1,54 @@
+import urllib
+from tqdm import tqdm
+from pathlib import Path
+
 import scraping
 import scrap_category
 import write_csv
-import urllib
-import os
-from tqdm import tqdm
+
+IMG_TITLE_MAX_LENGTH = 50
 
 
-def create_file_cat(home):
+def scrap_and_create_csv_files(home_page: str, data_dir: str = 'data') -> None:
     """
-    Loop through all categories,
-    creates a folder of the category if it doesn't exist and the csv inside
-    then for all books in the category extract and write the data on the csv.
-    :param home: home page
+    Scrape data for all categories, create a folder for each category if it doesn't exist,
+    and create a CSV file inside it. Then extract and write the data to the CSV.
+
+    :param home_page: The home page URL.
+    :param data_dir: The directory where data will be stored (default is 'data').
     :return: None
     """
-    print("Nombre de catégories dont le csv est créé")
-    for category in tqdm(scrap_category.scrap_category(home)):
-        if not os.path.exists('Data/'+category):
-            os.mkdir('Data/'+category)
+    print("Avancement du nombre de catégories dont le csv est créé")
+    for category in tqdm(scrap_category.scrap_category(home_page)):
+        category_path = Path(data_dir) / category
+        category_path.mkdir(parents=True, exist_ok=True)
         write_csv.init_category_csv(category)
         url_category = scrap_category.browse_page(
-            scrap_category.scrap_category(home)[category])
-        for livre in url_category:
+            scrap_category.scrap_category(home_page)[category])
+        for book in url_category:
             write_csv.write_csv(scraping.extract(
-                url_category[livre], category), category)
+                url_category[book], category), category)
 
 
-def images(home):
+def download_book_images(home_page: str, data_dir: str = 'data') -> None:
     """
-    Loop through all categories,
-    creates a folder of the category if it doesn't exist and the images folder inside
-    then on all the books in the category downloads in the images folder,
-    the image of each book by naming it by removing the
-    prohibited characters with a maximum of 50 characters
-    :param home: home page
+    Loop through all categories, create a folder for each category if it doesn't exist, along with
+    an 'Images' subfolder. Then download the image of each book in the category, naming it by
+    removing prohibited characters with a maximum of 50 characters.
+
+    :param home_page: The home page URL.
+    :param data_dir: The directory where data will be stored (default is 'data').
     :return: None
     """
-    print("Nombre de catégories dont les images sont téléchargées")
-    for category in tqdm(scrap_category.scrap_category(home)):
+    print("Avancement du nombre de catégories dont les images sont téléchargées")
+    for category in tqdm(scrap_category.scrap_category(home_page)):
+        category_path = Path(data_dir) / category / "Images"
+        category_path.mkdir(parents=True, exist_ok=True)
         url_category = scrap_category.browse_page(
-            scrap_category.scrap_category(home)[category])
-        if not os.path.exists("Data/"+category + "/Images"):
-            os.mkdir("Data/"+category + "/Images")
-        for livre in url_category:
-            format_title = scraping.format_title(livre)
-            urllib.request.urlretrieve(
-                scraping.extract(url_category[livre], category)
-                ['image_url'], "Data/"+category+"/Images/" + format_title[:min(
-                    50, len(str(format_title)))] + ".jpg")
+            scrap_category.scrap_category(home_page)[category])
+        for book in url_category:
+            format_title = scraping.format_title(book)
+            image_url = scraping.extract(url_category[book], category)['image_url']
+            image_name = format_title[:min(IMG_TITLE_MAX_LENGTH, len(str(format_title)))] + ".jpg"
+            image_path = category_path / image_name
+            urllib.request.urlretrieve(image_url, image_path)
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import download
 import os
 
+# Constantes
+BASE_URL = "http://books.toscrape.com/index.html"
+DATA_DIR = "data"
+
+
+def main():
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    download.scrap_and_create_csv_files(BASE_URL, data_dir=DATA_DIR)
+    download.download_book_images(BASE_URL, data_dir=DATA_DIR)
+
 
 if __name__ == "__main__":
-    home = "http://books.toscrape.com/index.html"
-    if not os.path.exists('Data'):
-        os.mkdir('Data')
-    download.create_file_cat(home)
-    download.images(home)
+    main()
+
+
+
