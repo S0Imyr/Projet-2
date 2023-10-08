bd401afe2d8a8483eb4a7d6f6ce54abb4d7eefb3
rlossec <raphaelossec@gmail.com>
2023-10-08 21:16:24 +0200

    Update exception in main.

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ BASE_URL = "http://books.toscrape.com/index.html"
 DATA_DIR = "data"
 
 
-def check_internet_connection(url):
+def check_connection(url: str) -> bool:
     """
     Check internet connectivity by trying to access a URL.
+
     :param url: The URL to check for internet connectivity.
     :return: True if there is an internet connection, False otherwise.
+    :raises requests.exceptions.RequestException: if there's an issue with the request
     """
     try:
         response = requests.get(url)
         response.raise_for_status()
         return True
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.RequestException:
         return False
 
 
 def main():
-    if not check_internet_connection(BASE_URL):
+    if not check_connection(BASE_URL):
         raise Exception("No internet connection available.")
 
     if not os.path.exists(DATA_DIR):
